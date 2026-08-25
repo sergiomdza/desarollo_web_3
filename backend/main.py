1d382678742b12f86e508a5efd7893cda0b79e1d
Eliam28 <152578969+Eliam28@users.noreply.github.com>
2026-08-24 19:19:14 -0600

    Clase 3 - 24 de agosto
    Falta levantar el contendor con la imagen creada

---
+++ backend/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from pymongo import MongoClient
+
+app = FastAPI()
+
+# mongo db connection
+mongo_client = MongoClient("mongodb://admin_user:web3@localhost:27017/")
+database = mongo_client["desarrollo_web_3"]
+productos = database["productos"]
+
+@app.get("/")
+def default():
+    return {"message":"Uvicorn server running"}
+
+@app.get("/health")
+def health_check():
+    return {"Status":"ok"}
+
+@app.get("/productos")
+def get_products():
+    return list(productos.find({}, {"_id": 0}))
