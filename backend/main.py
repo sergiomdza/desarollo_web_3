a7855db664f13a0fe88872de6f0d74b0fa80442f
Arturo7baldipro <153790614+Arturo7baldipro@users.noreply.github.com>
2026-08-24 19:16:58 -0600

    24-08-2026
    Agreacion de poetry con mongo, fastapi y uvicorn, y dockerfile en el backend

---
+++ backend/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from pymongo import MongoClient
+
+app = FastAPI()
+
+mongo_client = MongoClient("mongodb://admin_user:dfjkdfjk_2@localhost:27017/")
+database = mongo_client["desarrollo_web_3"]
+productos = database["productos"]
+
+@app.get("/")
+def default():
+    return {"status": "Uvicorn server running"}
+
+@app.get("/health")
+def health_check():
+    return {"status": "ok"}
+
+@app.get("/productos")
+def get_productos():
+    return list(productos.find({}, {"_id": 0}))
