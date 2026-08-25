2864df58820ed5a98efb09234a1761ef2140e60c
VxReaver <kuukgabi@outlook.com>
2026-08-24 19:26:09 -0600

    24/08/2026
    calse de esta fecha

---
+++ backend/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from pymongo import MongoClient
+
+app = FastAPI()
+
+# MONGO DB connection
+mongo_client = MongoClient ("mongodb://admin_user:web3@localhost:27017/")
+database = mongo_client["desarrollo_web_3"]
+productos = database["Productos"]
+
+@app.get("/health")
+def health_check():
+    return {"status": "ok"}
+
+@app.get("/")
+def default():
+    return {"message": "uvicorn server running"}
+
+@app.get("/productos")
+def get_productos():
+    return list(productos.find({}, {"_id": 0})) 
