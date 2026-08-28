2ffec6890f17007a1b97d027de135a7d521adabc
Emilio Campos <142188965+Touch89@users.noreply.github.com>
2026-08-28 17:29:36 -0600

    Commmit 1

---
+++ backend/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from pymongo import MongoClient
+
+app = FastAPI()
+
+# Mongo DB connection
+mongo_client = MongoClient("mongodb://admin_user:web3@mongo_container:27017/")
+database = mongo_client["desarrollo_web_3"]
+productos = database["productos"]
+
+
+@app.get("/")
+def root():
+    return {"message": "Uvicorn server running"}
+
+@app.get("/health")
+def health_check():
+    return {"status": "ok"}
+
+
+@app.get("/productos")
+def get_productos():
+    return list(productos.find({}, {"_id": 0}))
+
