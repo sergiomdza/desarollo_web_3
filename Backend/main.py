154fb841b5085e50807189149fcf7e91b3edf706
Julio Traconiz <jftso415@hotmail.com>
2026-08-28 17:30:08 -0600

    Primer commit, primera parte del backend

---
+++ Backend/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from pymongo import MongoClient
+
+app = FastAPI()
+
+#mongo db connection
+mongo_client = MongoClient("mongodb://admin_user:web3@localhost:27017/")
+database = mongo_client["desarrollo_web_3"]
+productos = database["productos"]
+
+@app.get("/")
+def default():
+    return {"message": "Corriendo"}
+
+@app.get("/health")
+def health_check():
+    return {"status": "ok"}
+
+@app.get("/productos")
+def get_productos():
+    return list(productos.find({}, {"_id": 0}))
