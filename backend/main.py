2114f41554f1fac413f96d125475959271c2c81c
Scorpion-CC <carloscantom@gmail.com>
2026-08-31 17:22:55 -0600

    Evidencias Clase 28/08/26
    Avances del proyecto. Se modificó el docker-compose y el dockerfile.

---
+++ backend/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from pymongo import MongoClient
+
+app = FastAPI()
+
+# Mongo DB connection
+mongo_client = MongoClient("mongodb://admin_user:web3@localhost:27017/")
+database = mongo_client["desarrollo_web_3"]
+productos = database["productos"]
+
+@app.get("/")
+def default():
+    return {"message": "HOLA MUNDO"}
+
+@app.get("/health")
+def health_check():
+    return {"status": "ok"}
+
+@app.get("/productos")
+def get_productos():
+    return list(productos.find({}, {"_id": 0}))
