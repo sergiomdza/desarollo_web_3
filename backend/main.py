5e38a0764236c755c06aee6a92375f5c7c705294
AngelRosadoWTF <angsoft05@gmail.com>
2026-08-31 16:40:17 -0600

    Evidencia 3
    28/08/2026

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from pymongo import MongoClient
 
 app = FastAPI()
-mongo_client = MongoClient("mongodb://admin_user:web3@localhost:27017/")
+mongo_client = MongoClient("mongodb://admin_user:web3@mongo_container:27017/")
 database = mongo_client["desarrollo_web_3"]
 productos = database["productos"]
 
