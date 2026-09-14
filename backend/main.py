28ac3a71a8197b459ec0fa5479cab993a78a0219
Scorpion-CC <carloscantom@gmail.com>
2026-09-14 17:13:29 -0600

    Evidencias Clase 11/09/26
    Se añadió un servicio al mongo_statefulset y se cambió la dirección del mongo_client en main.py

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 app = FastAPI()
 
 # Mongo DB connection
-mongo_client = MongoClient("mongodb://admin_user:web3@localhost:27017/")
+mongo_client = MongoClient("mongodb://admin_user:web3@mongo-service.default.svc.cluster.local:27017/")
 database = mongo_client["desarrollo_web_3"]
 productos = database["productos"]
 
