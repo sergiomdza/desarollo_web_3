e1fe11034ba641e39c55aa83650b9aa99b167dcf
ᓚᘏᗢ !? <datamayo.211@gmail.com>
2026-09-14 19:00:45 -0600

    14/09

--- Kubernetes/setup_app.sh
+++ Kubernetes/setup_app.sh
@@ -3,5 +3,5 @@
 kind load docker-image mongo:7.0 --name web3
 kind load docker-image backend_productos:latest --name web3
 
-kubectl apply -f ./mongo_statefulset.yaml
-kubectl apply -f ./backend_deployment.yaml
+kubectl apply -f ./kubernetes/mongo_statefulset.yaml
+kubectl apply -f ./kubernetes/backend_deployment.yaml
