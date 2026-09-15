269f374389bc6e9c18d704033a72d5d1826ea7ee
Sergio Mendoza <sergio.mendoza@verato.com>
2026-09-14 19:05:34 -0600

    testing part 2

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,7 @@ productos = database["productos"]
 
 @app.get("/")
 def default():
-    return {"message": "HOLA MUNDO"}
+    return {"message": "SE ACABO LA CLASE"}
 
 @app.get("/health")
 def health_check():
