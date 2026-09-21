07f1a88cacd52802cd9624a5ee8b2fa5d5d3fe09
Scorpion-CC <carloscantom@gmail.com>
2026-09-21 14:05:07 -0600

    Evidencias Clase 14/09/26
    Se añadió la carpeta del github actions para crear imágenes de los cambios que vayamos haciendo al avanzar con el proyecto. También se hicieron correcciones en algunos archivos.

--- Kubernetes/setup_app.sh
+++ Kubernetes/setup_app.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+#docker build --no-cache -t desarollo_web_3-backend:latest .
+
 kind load docker-image mongo:7.0 --name web3
-kind load docker-image backend_productos:latest --name web3
+kind load docker-image desarollo_web_3-backend:latest --name web3
 
 kubectl apply -f ./mongo_statefulset.yaml
 kubectl apply -f ./backend_deployment.yaml
